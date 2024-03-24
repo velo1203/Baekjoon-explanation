cb8a49613d02fd38cc7b1790b45d63a3804699a4
velo1203 <hokitoki1203@gmail.com>
2024-03-24 09:27:11 +0900

    add

--- 브론즈/11022.py
+++ 브론즈/11022.py
@@ -3,4 +3,4 @@ testcase = int(sys.stdin.readline())
 for n in range(testcase):
     nums = sys.stdin.readline()
     (x,y) = map(int,nums.split())
-    print(f"Case #{n+1}: {x+y}")
+    print(f"Case #{n+1}: {x} + {y} = {x+y}")
