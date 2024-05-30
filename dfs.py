9899d6d6de27bec0f5c78e66304a7db21a527ec9
velo1203 <hokitoki1203@gmail.com>
2024-05-30 11:20:52 +0900

    add

---
+++ dfs.py
@@ -0,0 +1,12 @@
+graph = dict()
+ 
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
