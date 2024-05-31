e9b9249702c86d9419e18c1d9ed8497d09af3fa9
velo1203 <hokitoki1203@gmail.com>
2024-05-31 18:28:02 +0900

    add

--- codes/4134.py
+++ codes/4134.py
@@ -1,19 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-length = int(input())
+length = int(input().strip())
 for n in range(length): #테스트 케이스
 
-    num = n
+    num = int(input().strip())
+    if num == 0 or num ==1:
+        print(2)
+        continue
     while True:
         isprime = True
-        for number in range(2,int(num**0.5) + 1):
-            if num%number != 0:
+        for number in range(2, int(num**0.5) + 1):
+            if num % number == 0:
                 isprime = False
                 break
             
-        num+=1
-        if isprime == True:
-            print(number)
+        if isprime:
+            print(num)
             break
         
+        num += 1
